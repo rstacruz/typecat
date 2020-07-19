149983dc25df738a902944e38863c563bdc94af2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-07-19 22:41:38 +1000

    Prerender CSS

---
+++ pages/_document.tsx
@@ -0,0 +1,46 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+// https://github.com/vercel/next-plugins/issues/238#issuecomment-432211871
+class InlineStylesHead extends Head {
+  getCssLinks() {
+    if (process.env.NODE_ENV === 'production') {
+      return this.__getInlineStyles()
+    } else {
+      return []
+    }
+  }
+
+  __getInlineStyles() {
+    const { assetPrefix, files } = this.context._documentProps
+    if (!files || files.length === 0) return null
+
+    return files
+      .filter((file) => /\.css$/.test(file))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: readFileSync(join(process.cwd(), '.next', file), 'utf-8'),
+          }}
+        />
+      ))
+  }
+}
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <html>
+        <InlineStylesHead />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
