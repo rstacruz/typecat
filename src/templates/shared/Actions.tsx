4bfe7cd42f65a105dee33f73871c33087d6b02a2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-07-20 22:17:20 +1000

    Implement Actions as a class

---
+++ src/templates/shared/Actions.tsx
@@ -0,0 +1,150 @@
+import { State, Result, Token } from './useStore'
+import tokenize from '../AppPage/SessionUI/TextDisplay/useStore/tokenize'
+import generateEnglish from '../AppPage/SessionUI/TextDisplay/useStore/generateEnglish'
+
+class Actions {
+  update: any
+
+  constructor(update: any) {
+    this.update = update
+  }
+
+  startNewSession = (): void => {
+    const { update } = this
+
+    update(({ state }) => {
+      resetSession(state)
+
+      setTimeout(() => {
+        update(({ state }) => {
+          loadNewSession(state)
+        })
+      }, 300)
+    })
+  }
+
+  setInputValue = (value: string): void => {
+    const { update } = this
+
+    update(({ state }) => {
+      setInputValue(state, value)
+    })
+  }
+
+  inputWhitespace = (): void => {
+    const { update } = this
+
+    update(({ state }) => {
+      // Only if a session's started
+      if (state.session.status !== 'ongoing') return
+
+      const index = state.currentInput.tokenIndex
+      const token = state.article.tokens[index]
+      const currentValue = state.currentInput.value
+
+      // Double enter
+      if (currentValue === '') {
+        state.currentInput.charIndex = 0
+        return
+      }
+
+      // Check for accuracy
+      let isAccurate = token && token.value === currentValue
+
+      // Mark as done
+      state.currentInput.finishedTokens[index] = {
+        isAccurate: !!isAccurate,
+      }
+
+      // Are we done?
+      const nextIndex = state.currentInput.tokenIndex + 2
+      if (nextIndex > state.article.tokens.length) {
+        generateResults(state)
+        resetSession(state)
+        loadNewSession(state)
+      } else {
+        // TODO: Skip over any whitespace nodes
+        state.currentInput.tokenIndex = nextIndex
+        state.currentInput.charIndex = 0
+        state.currentInput.value = ''
+        state.currentInput.isAccurate = true
+      }
+    })
+  }
+}
+
+/**
+ * Generates results.
+ */
+
+export function generateResults(state: State): void {
+  const wpm = Math.round(60 + 30 * Math.random())
+  const result: Result = { wpm, accuracy: 0.98 }
+  state.results.push(result)
+}
+
+/**
+ * Resets the current session.
+ */
+
+export function resetSession(state: State): void {
+  state.session = { status: 'pending' }
+
+  state.currentInput = {
+    tokenIndex: 0,
+    charIndex: 0,
+    value: '',
+    isAccurate: true,
+    finishedTokens: [],
+  }
+
+  state.article.tokens = []
+}
+
+export function loadNewSession(state: State): void {
+  state.session = { status: 'ready' }
+
+  state.currentInput = {
+    tokenIndex: 0,
+    charIndex: 0,
+    value: '',
+    isAccurate: true,
+    finishedTokens: [],
+  }
+
+  state.article.tokens = tokenize(
+    generateEnglish(Math.round(Math.random() * 10))
+  )
+}
+
+/**
+ * Returns the current token.
+ */
+
+function getCurrentToken(state: State): Token {
+  return state.article.tokens[state.currentInput.tokenIndex]
+}
+
+/**
+ * Updates an input value.
+ */
+
+export function setInputValue(state: State, value: string) {
+  const token = getCurrentToken(state)
+
+  // Start if not started
+  if (state.session.status === 'ready') {
+    state.session = { status: 'ongoing', startedAt: new Date() }
+  }
+
+  if (token && token.value && token.value.substr(0, value.length) !== value) {
+    state.currentInput.isAccurate = false
+  } else {
+    state.currentInput.isAccurate = true
+  }
+
+  state.currentInput.value = value
+  state.currentInput.charIndex = value.length
+}
+
+export default Actions
--- src/templates/shared/useStore.tsx
+++ src/templates/shared/useStore.tsx
@@ -2,6 +2,7 @@ import create from 'zustand'
 import produce from 'immer'
 import tokenize from '../AppPage/SessionUI/TextDisplay/useStore/tokenize'
 import generateEnglish from '../AppPage/SessionUI/TextDisplay/useStore/generateEnglish'
+import Actions from './Actions'
 
 export type Token = {
   type: string
@@ -44,11 +45,7 @@ export type Store = {
       finishedTokens: (TokenStatus | null | void)[]
     }
   }
-  actions: {
-    startNewSession: () => void
-    setInputValue: (value: string) => void
-    inputWhitespace: () => void
-  }
+  actions: Actions
 }
 
 export type State = Store['state']
@@ -76,139 +73,9 @@ const [useStore] = create<Store>((set) => {
     },
   }
 
-  const actions: Store['actions'] = {
-    startNewSession() {
-      update(({ state }) => {
-        resetSession(state)
-        setTimeout(() => {
-          update(({ state }) => {
-            loadNewSession(state)
-          })
-        }, 300)
-      })
-    },
-
-    setInputValue(value) {
-      update(({ state }) => {
-        setInputValue(state, value)
-      })
-    },
-
-    inputWhitespace() {
-      update(({ state }) => {
-        // Only if a session's started
-        if (state.session.status !== 'ongoing') return
-
-        const index = state.currentInput.tokenIndex
-        const token = state.article.tokens[index]
-        const currentValue = state.currentInput.value
-
-        // Double enter
-        if (currentValue === '') {
-          state.currentInput.charIndex = 0
-          return
-        }
-
-        // Check for accuracy
-        let isAccurate = token && token.value === currentValue
-
-        // Mark as done
-        state.currentInput.finishedTokens[index] = {
-          isAccurate: !!isAccurate,
-        }
-
-        // Are we done?
-        const nextIndex = state.currentInput.tokenIndex + 2
-        if (nextIndex > state.article.tokens.length) {
-          generateResults(state)
-          resetSession(state)
-          loadNewSession(state)
-        } else {
-          // TODO: Skip over any whitespace nodes
-          state.currentInput.tokenIndex = nextIndex
-          state.currentInput.charIndex = 0
-          state.currentInput.value = ''
-          state.currentInput.isAccurate = true
-        }
-      })
-    },
-  }
+  const actions = new Actions(update)
 
   return { state, actions }
 })
-
-/**
- * Generates results.
- */
-
-function generateResults(state: State): void {
-  const wpm = Math.round(60 + 30 * Math.random())
-  const result: Result = { wpm, accuracy: 0.98 }
-  state.results.push(result)
-}
-
-/**
- * Resets the current session.
- */
-
-function resetSession(state: State): void {
-  state.session = { status: 'pending' }
-
-  state.currentInput = {
-    tokenIndex: 0,
-    charIndex: 0,
-    value: '',
-    isAccurate: true,
-    finishedTokens: [],
-  }
-
-  state.article.tokens = []
-}
-
-function loadNewSession(state: State): void {
-  state.session = { status: 'ready' }
-
-  state.currentInput = {
-    tokenIndex: 0,
-    charIndex: 0,
-    value: '',
-    isAccurate: true,
-    finishedTokens: [],
-  }
-
-  state.article.tokens = tokenize(
-    generateEnglish(Math.round(Math.random() * 10))
-  )
-}
-
-/**
- * Returns the current token.
- */
-
-function getCurrentToken(state: State): Token {
-  return state.article.tokens[state.currentInput.tokenIndex]
-}
-
-/**
- * Updates an input value.
- */
-
-function setInputValue(state: State, value: string) {
-  const token = getCurrentToken(state)
-
-  // Start if not started
-  if (state.session.status === 'ready') {
-    state.session = { status: 'ongoing', startedAt: new Date() }
-  }
-
-  if (token && token.value && token.value.substr(0, value.length) !== value) {
-    state.currentInput.isAccurate = false
-  } else {
-    state.currentInput.isAccurate = true
-  }
-
-  state.currentInput.value = value
-  state.currentInput.charIndex = value.length
-}
 
 export default useStore
