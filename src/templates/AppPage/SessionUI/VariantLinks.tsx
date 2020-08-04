67dd828c1eae0581d44fe1b8faf6188001d6f8dd
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-08-05 01:28:31 +1000

    Fix variant links

--- src/templates/AppPage/SessionUI/VariantLinks.tsx
+++ src/templates/AppPage/SessionUI/VariantLinks.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
 import CSS from './VariantLinks.module.css'
+import { LanguageName } from '../../../store/useStore'
 
 export function VariantLinks() {
   const wordCounts = [25, 50, 100]
+  const language: LanguageName = 'english'
   return (
     <nav className={CSS.root}>
       {wordCounts.map((wordCount) => (
         <span key={wordCount} className={CSS.item}>
           <Link
-            href='/english/[wordCount]'
-            as={`/english/${wordCount}`}
+            href='/[language]/[wordCount]'
+            as={`/${language}/${wordCount}`}
             replace
           >
             <a>{wordCount}</a>
