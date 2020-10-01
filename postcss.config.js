e36705dfdaca75dff40ceb96b10dc3336d3ac508
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-10-02 00:54:33 +1000

    Make a totally useless change

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,7 @@ const { resolve } = require('path')
 
 module.exports = {
   plugins: [
-    [
-      'postcss-import',
-      {
-        path: resolve(__dirname, 'src'),
-      },
-    ],
+    ['postcss-import', { path: resolve(__dirname, 'src') }],
     'tailwindcss',
     ['postcss-preset-env', { stage: 1 }],
   ],
